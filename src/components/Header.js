9c2743d980eaf09ac822f64c82d18add161a5de4
william lee <will.lee.npc@gmail.com>
2023-12-26 00:34:25 -0500

    complete lab

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header({onDarkModeClick, mode}) {
+
+return (
+    
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {mode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+      
+  );
+}
+
+  export default Header
